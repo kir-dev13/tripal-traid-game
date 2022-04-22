c6e9f12cec5aeee0ff7eea41c6f7d3abe413980b
kir-dev13 <kirill87rabota@gmail.com>
2022-04-22 15:09:38 +0300

    homework-4 process

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,11 @@ import cn from "classnames";
 
 import s from "./Button.module.scss";
 
-const Button = ({ className, black, children }) => {
+const Button = ({ className, black, children, onClick }) => {
     return (
         <div className={s.root}>
             <button
+                onClick={onClick}
                 className={cn(s.button, className, {
                     [s.colorBlack]: black,
                 })}
